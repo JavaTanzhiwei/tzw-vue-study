98cd1b456b008adbe03a221ff0662512af4bb5ef
tanzhiwei <tanzhiwei0526@163.com>
2022-10-18 16:02:28 +0800

    日志

--- src/api/system/logininfor.js
+++ src/api/system/logininfor.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询登录日志列表
 export function list(query) {
   return request({
-    url: '/system/logininfor/list',
+    url: '/tzwSystem/log/login/queryPageLoginLogList',
     method: 'get',
     params: query
   })
@@ -12,22 +12,15 @@ export function list(query) {
 // 删除登录日志
 export function delLogininfor(infoId) {
   return request({
-    url: '/system/logininfor/' + infoId,
+    url: '/tzwSystem/log/login/' + infoId,
     method: 'delete'
   })
 }
 
-// 解锁用户登录状态
-export function unlockLogininfor(userName) {
-  return request({
-    url: '/system/logininfor/unlock/' + userName,
-    method: 'get'
-  })
-}
 // 清空登录日志
 export function cleanLogininfor() {
   return request({
-    url: '/system/logininfor/clean',
+    url: '/tzwSystem/log/login/clean',
     method: 'delete'
   })
 }
